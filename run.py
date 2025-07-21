98c625e8fc6b7ddcee55f6055bdaa8ecfbabd2e0
rinkutmk93 <rinkutmk69@gmail.com>
2025-07-21 16:02:58 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import os,sys,platform
 bit = platform.architecture()[0]
 if bit == '64bit':
-    os.system('chmod +x tbg')
-    os.system('./tbg')
+    os.system('chmod +x tmk')
+    os.system('./tmk')
 elif bit == '32bit':
-    os.system('chmod +x tbg32')
-    os.system('./tbg32')
+    os.system('chmod +x tmk32')
+    os.system('./tmk32')
 else:
     print('device not supported')
